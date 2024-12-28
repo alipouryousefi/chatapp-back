1e48c34bc39c132c4d60072ba64850a78a2e5d93
Ali Pouryousefi <a.pouryousefi98@gmail.com>
2024-12-28 17:39:16 +0330

    add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import environmentValidation from './config/environment.validation';
 
+
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [appConfig, databaseConfig],
+      validationSchema: environmentValidation,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
