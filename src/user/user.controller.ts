7e7fe92788c7668acb78808bfc732ac87ea98510
Ali Pouryousefi <a.pouryousefi98@gmail.com>
2025-01-13 17:16:35 +0330

    add user search

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('search')
+  async searchUser(@Query('query') query: string) {
+    return this.userService.searchUser(query);
+  }
+  
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Like, Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async searchUser(query: string): Promise<User[]> {
+    return this.userRepository.find({
+      where: [{ username: Like(`%${query}%`) }, { email: Like(`%${query}%`) }],
+    });
+  }
+
+  
+}
